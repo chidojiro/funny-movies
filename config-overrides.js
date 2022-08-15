0ca19897828c402f36b3db3294a3223877e3e605
Nick <chidojiro@gmail.com>
2022-08-16 06:34:15 +0700

    feat: add common hooks

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = function override(config, env) {
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, 'src'),
+  };
+
+  return config;
+};
---
+++ src/common/hooks/useFetcher/useFetcher.ts
@@ -0,0 +1,34 @@
+import React from 'react';
+import useSwr, { SWRConfiguration } from 'swr';
+
+export type UseFetcherConfiguration<T> = SWRConfiguration<T> & {
+  laggy?: boolean;
+};
+
+export const useFetcher = <T = unknown>(
+  key: string | unknown[] | null | undefined | false,
+  callback: (...args: unknown[]) => Promise<T>,
+  config?: UseFetcherConfiguration<T>
+) => {
+  const { laggy, ...restConfig } = config ?? {};
+  const laggyDataRef = React.useRef<T>();
+  const swrReturn = useSwr<T>(
+    key,
+    async () => {
+      const data = await callback();
+      laggyDataRef.current = data;
+      return data;
+    },
+    restConfig
+  );
+
+  return React.useMemo(
+    () => ({
+      ...swrReturn,
+      data: swrReturn.data ?? (laggy ? laggyDataRef.current : undefined),
+      isLagging: swrReturn.data === undefined && laggyDataRef.current !== undefined,
+      isInitializing: !swrReturn.error && !swrReturn.data && swrReturn.isValidating,
+    }),
+    [laggy, swrReturn]
+  );
+};
