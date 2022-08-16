ab637d2089a494dcf338f784218b5355fae12716
Nick <chidojiro@gmail.com>
2022-08-16 21:21:21 +0700

    fix: incorrect cookie access token key

--- src/auth/useAccessTokenCookieState.ts
+++ src/auth/useAccessTokenCookieState.ts
@@ -1,3 +1,4 @@
 import { useCookieState } from '@/common/hooks';
+import { COOKIE_ACCESS_TOKEN_KEY } from './constants';
 
-export const useAccessTokenCookieState = () => useCookieState('token', '');
+export const useAccessTokenCookieState = () => useCookieState(COOKIE_ACCESS_TOKEN_KEY, '');
