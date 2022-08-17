ca439e594d7f642a060e95788629bbbb63f06534
Nick <chidojiro@gmail.com>
2022-08-17 12:17:47 +0700

    fix: add more padding to main layout content

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className={clsx(StringUtils.withProjectClassNamePrefix('main-layout'), 'max-w-[1600px] mx-auto')}>
       <MainLayoutHeader />
-      <div className='max-w-[1280px] py-10 mx-auto'>{children}</div>
+      <div className='max-w-[1280px] py-10 mx-auto px-4'>{children}</div>
     </div>
   );
 };
