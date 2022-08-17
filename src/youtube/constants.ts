4836e15bf219c0f3a2549ce96ddc5bea77b71b46
Nick <chidojiro@gmail.com>
2022-08-17 12:13:29 +0700

    fix: correct env variable

--- src/youtube/constants.ts
+++ src/youtube/constants.ts
@@ -1,3 +1,3 @@
-export const YOUTUBE_API_KEY = process.env.REACT_APP_OUTUBE_API_KEY;
+export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export const YOUTUBE__VIDEO_ENDPOINT = 'https://www.googleapis.com/youtube/v3/videos';
